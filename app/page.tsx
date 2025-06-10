2281c7e8eb784f7b5ab25c0cf1e57d0d8acd7cde
daniyarmukhanov <mukhanov.daniyar@gmail.com>
2025-06-10 15:03:50 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import ControlButton from "./_components/button/control-button";
+import Grid from "./_components/game/grid";
+import GameLostModal from "./_components/modal/game-lost-modal";
+import GameWonModal from "./_components/modal/game-won-modal";
+import Popup from "./_components/popup";
+import useAnimation from "./_hooks/use-animation";
+import useGameLogic from "./_hooks/use-game-logic";
+import usePopup from "./_hooks/use-popup";
+import { SubmitResult, Word } from "./_types";
+import { getPerfection } from "./_utils";
+
+export default function Home() {
+  const [popupState, showPopup] = usePopup();
+  const {
+    gameWords,
+    selectedWords,
+    clearedCategories,
+    mistakesRemaining,
+    isWon,
+    isLost,
+    guessHistoryRef,
+    selectWord,
+    shuffleWords,
+    deselectAllWords,
+    getSubmitResult,
+    handleWin,
+    handleLoss,
+  } = useGameLogic();
+
+  const [showGameWonModal, setShowGameWonModal] = useState(false);
+  const [showGameLostModal, setShowGameLostModal] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const {
+    guessAnimationState,
+    wrongGuessAnimationState,
+    animateGuess,
+    animateWrongGuess,
+  } = useAnimation();
+
+  const handleSubmit = async () => {
+    setSubmitted(true);
+    await animateGuess(selectedWords);
+
+    const result: SubmitResult = getSubmitResult();
+
+    switch (result.result) {
+      case "same":
+        showPopup("Бұндай жауап тапсырған болатынсыз");
+        break;
+      case "one-away":
+        animateWrongGuess();
+        showPopup("Бір ғана сөз қате...");
+        break;
+      case "loss":
+        showPopup("Келесі жолы байқап көріңіз! Келесі сұрақ ертең болады");
+        await handleLoss();
+        setShowGameLostModal(true);
+        break;
+      case "win":
+        showPopup(getPerfection(mistakesRemaining));
+        await handleWin();
+        setShowGameWonModal(true);
+        break;
+      case "incorrect":
+        animateWrongGuess();
+        break;
+    }
+    setSubmitted(false);
+  };
+
+  const onClickCell = useCallback(
+    (word: Word) => {
+      selectWord(word);
+    },
+    [selectWord]
+  );
+
+  const renderControlButtons = () => {
+    const showResultsWonButton = (
+      <ControlButton
+        text="Нәтижені көрсету"
+        onClick={() => {
+          setShowGameWonModal(true);
+        }}
+      />
+    );
+
+    const showResultsLostButton = (
+      <ControlButton
+        text="Нәтижені көрсету"
+        onClick={() => {
+          setShowGameLostModal(true);
+        }}
+      />
+    );
+
+    const inProgressButtons = (
+      <div className="flex gap-2 mb-12">
+        <ControlButton
+          text="Араластыру"
+          onClick={shuffleWords}
+          unclickable={submitted}
+        />
+        <ControlButton
+          text="Таңдауды тазалау"
+          onClick={deselectAllWords}
+          unclickable={selectedWords.length === 0 || submitted}
+        />
+        <ControlButton
+          text="Тапсыру"
+          unclickable={selectedWords.length !== 4 || submitted}
+          onClick={handleSubmit}
+        />
+      </div>
+    );
+
+    if (isWon) {
+      return showResultsWonButton;
+    } else if (isLost) {
+      return showResultsLostButton;
+    } else {
+      return inProgressButtons;
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center w-11/12 md:w-3/4 lg:w-7/12 mx-auto mt-14">
+        <h1 className="text-black text-4xl font-semibold my-4 ml-4">
+          Байланыс (aka Connections NYT)
+        </h1>
+        <hr className="mb-4 md:mb-4 w-full"></hr>
+        <h1 className="text-black mb-4">Бірдей топқа жататын төрт сөзді табыңыз!</h1>
+        <div className="relative w-full">
+          <Popup show={popupState.show} message={popupState.message} />
+          <Grid
+            words={gameWords}
+            selectedWords={selectedWords}
+            onClick={onClickCell}
+            clearedCategories={clearedCategories}
+            guessAnimationState={guessAnimationState}
+            wrongGuessAnimationState={wrongGuessAnimationState}
+          />
+        </div>
+        <h2 className="text-black my-4 md:my-8 mx-8">
+          Мүмкіндіктер саны:{" "}
+          {mistakesRemaining > 0 ? Array(mistakesRemaining).fill("•") : ""}
+        </h2>
+        {renderControlButtons()}
+      </div>
+      <GameWonModal
+        isOpen={showGameWonModal}
+        onClose={() => setShowGameWonModal(false)}
+        guessHistory={guessHistoryRef.current}
+        perfection={getPerfection(mistakesRemaining)}
+      />
+      <GameLostModal
+        isOpen={showGameLostModal}
+        onClose={() => setShowGameLostModal(false)}
+        guessHistory={guessHistoryRef.current}
+      />
+    </>
+  );
+}
