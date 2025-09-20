560d62f24f06144e03b41b9debf445f51ea8b3b3
Daniyar Mukhanov <daniyarmukhanov@users.noreply.github.com>
2025-09-20 09:45:16 +0500

    Change heading text style and content

--- app/page.tsx
+++ app/page.tsx
@@ -130,8 +130,8 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center w-11/12 md:w-3/4 lg:w-7/12 mx-auto mt-14">
-        <h1 className="text-black text-4xl font-semibold my-4 ml-4">
-          Төртеу Түгел
+        <h1 className="text-black text-4xl font-bold my-4 ml-4">
+          ТӨРТЕУ ТҮГЕЛ
         </h1>
         <hr className="mb-4 md:mb-4 w-full"></hr>
         <h1 className="text-black mb-4">Бірдей топқа жататын төрт сөзді табыңыз!</h1>
